a9cffc98a2ab8603877612dfa7571293bc8d4d0a
Gourang Jain <gourangjain7@gmail.com>
2022-08-09 17:44:57 +0530

    added media query

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ window.onbeforeunload = function () {
   });
 }
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("active")
+  navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll("nav-links").forEach(n => n.addEventListener("click", () =>{
+  hamburger.classList.remove("active")
+  navMenu.classList.remove("active")
+}))
+
+
 
 let timeline = gsap.timeline();
 
